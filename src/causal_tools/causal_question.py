a2fb8308f37b504a99cf4f9d7b24f04baa16fdab
abemo <abemooreodell@gmail.com>
2023-05-30 10:45:33 -0700

    fixed naming in causal_question.py

--- src/causal_tools/causal_question.py
+++ src/causal_tools/causal_question.py
@@ -16,11 +16,11 @@ class CausalQuestion:
     def __init__(self, nodes: list):
         self.nodes = nodes
         self.possible_queries = []
-        self.possible_queries.append(self.Remove_Edge_Q(node, edge))
-        self.possible_queries.append(self.Add_Edge_Q(node, edge))
-        self.possible_queries.append(self.Do_Q(node, set_value))
-        self.possible_queries.append(self.Remove_Node_Q(node))
-        self.possible_queries.append(self.Add_Node_Q(node))
+        self.possible_queries.append(self.RemoveEdgeQ(node, edge))
+        self.possible_queries.append(self.AddEdgeQ(node, edge))
+        self.possible_queries.append(self.SetNodeQ(node, set_value))
+        self.possible_queries.append(self.RemoveNodeQ(node))
+        self.possible_queries.append(self.AddNodeQ(node))
 
     def best_query(self):
         return max(self.possible_queries, key=lambda q: q.entropy)
@@ -30,28 +30,28 @@ class Question:
     def __init__(self, type: str, node, edge):
         self.node = node
         self.edge = edge
-                
+
     
-    class Remove_Edge_Q:
+    class RemoveEdgeQ:
         def __init__(self, node, edge):
             self.node = node
             self.edge = edge
             
-    class Add_Edge_Q:
+    class AddEdgeQ:
         def __init__(self, node, edge):
             self.node = node
             self.edge = edge
 
-    class Do_Q:
+    class SetNodeQ:
         def __init__(self, node, set_value=randint.choice([0, 1])):
             self.node = node
             self.set_values = set_value
     
-    class Remove_Node_Q:
+    class RemoveNodeQ:
         def __init__(self, node):
             self.node = node
 
-    class Add_Node_Q:
+    class AddNodeQ:
         def __init__(self, node):
             self.node = node
 
