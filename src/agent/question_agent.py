e4ea9d47ec67e5b78dc54001e291da2671116ffc
abemo <abemooreodell@gmail.com>
2023-05-30 11:26:54 -0700

    created QuestionAgent class

---
+++ src/agent/question_agent.py
@@ -0,0 +1,16 @@
+"""
+A MAB agent for finding the best questions to ask of a model
+Use a combination of:
+ -- entropy
+ -- expected reward
+"""
+
+class QuestionAgent:
+    def __init__(self): 
+        pass
+
+    def ask_question(self, model):
+        """
+        ask the best question of the model
+        """
+        pass
