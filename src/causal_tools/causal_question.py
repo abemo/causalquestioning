a129631ff7baca411e61dcb96ee43fec06be89aa
abemo <abemooreodell@gmail.com>
2023-05-30 10:43:39 -0700

    updated causal_question.py

--- src/causal_tools/causal_question.py
+++ src/causal_tools/causal_question.py
@@ -9,19 +9,51 @@
 """
 
 import scm
+from random import randint, choice
 
 
 class CausalQuestion:
-    def __init__(self, node):
-        self.node = node
+    def __init__(self, nodes: list):
+        self.nodes = nodes
         self.possible_queries = []
+        self.possible_queries.append(self.Remove_Edge_Q(node, edge))
+        self.possible_queries.append(self.Add_Edge_Q(node, edge))
+        self.possible_queries.append(self.Do_Q(node, set_value))
+        self.possible_queries.append(self.Remove_Node_Q(node))
+        self.possible_queries.append(self.Add_Node_Q(node))
 
     def best_query(self):
-        entropy = scm.node_entropy(self.node)
-        pass
+        return max(self.possible_queries, key=lambda q: q.entropy)
+        
 
-    def __repr__(self):
-        pass
+class Question:
+    def __init__(self, type: str, node, edge):
+        self.node = node
+        self.edge = edge
+                
+    
+    class Remove_Edge_Q:
+        def __init__(self, node, edge):
+            self.node = node
+            self.edge = edge
+            
+    class Add_Edge_Q:
+        def __init__(self, node, edge):
+            self.node = node
+            self.edge = edge
+
+    class Do_Q:
+        def __init__(self, node, set_value=randint.choice([0, 1])):
+            self.node = node
+            self.set_values = set_value
+    
+    class Remove_Node_Q:
+        def __init__(self, node):
+            self.node = node
+
+    class Add_Node_Q:
+        def __init__(self, node):
+            self.node = node
 
 
 
